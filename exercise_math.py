07d4e4b06240036f25fa49f439e98edde164db72
isamastronardi <imastronardibelpoli@mail.austral.edu.ar>
2025-08-20 13:50:33 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+    suma= a +b 
+    print(suma)
+    diferencia= a-b
+    print(diferencia)
+    producto= a*b
+    print(producto)
+    promedio= (a+b)/2
+    print(promedio)
+    division_entera= a//b
+    print(division_entera)
+    resto= a%b
+    print(resto)
+    division= a/b
+    print(division)
+
+#math()
